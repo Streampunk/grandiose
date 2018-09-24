a9ccb947ce77eb91aba1b1cf126a377088d63104
Richard Cartwright <spark@streampunk.media>
2018-09-24 18:57:50 +0100

    Passing parameters through to find method. Still not MDNS-ing properly :-(

--- index.js
+++ index.js
@@ -18,4 +18,19 @@ const addon = require('bindings')('grandiose');
 const SegfaultHandler = require('segfault-handler');
 SegfaultHandler.registerHandler("crash.log"); // With no argument, SegfaultHandler will generate a generic log file name
 
-module.exports = addon;
+let find = function (args) {
+  if (args == undefined) return addon.find();
+  if (Array.isArray(args.groups)) {
+    args.groups = args.groups.reduce((x, y) => x + ',' + y);
+  }
+  if (Array.isArray(args.extraIPs)) {
+    args.extraIPs = args.extraIPs.reduce((x, y) => x + ',' + y);
+  }
+  return addon.find(args);
+}
+
+module.exports = {
+  version: addon.version,
+  find: find,
+  isSupportedCPU: addon.isSupportedCPU
+};
--- src/grandiose.cc
+++ src/grandiose.cc
@@ -24,15 +24,88 @@ napi_value version(napi_env env, napi_callback_info info) {
   return result;
 }
 
+napi_value isSupportedCPU(napi_env env, napi_callback_info info) {
+  napi_status status;
+
+  napi_value result;
+  status = napi_get_boolean(env, NDIlib_is_supported_CPU(), &result);
+  CHECK_STATUS;
+
+  return result;
+}
+
+// MDNS does not appear to be working within Node, OK (as MDNS ever is) autside
 napi_value find(napi_env env, napi_callback_info info) {
   napi_status status;
 
-  // if (!NDIlib_initialize()) NAPI_THROW_ERROR("Failed to inittialise NDI subsystem.");
+  if (!NDIlib_initialize()) NAPI_THROW_ERROR("Failed to inittialise NDI subsystem.");
 
   NDIlib_find_create_t find_create;
   find_create.show_local_sources = true;
   find_create.p_groups = nullptr;
   find_create.p_extra_ips = nullptr;
+
+  size_t argc = 1;
+  napi_value argv[1];
+  status = napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
+  CHECK_STATUS;
+  if (argc >= 1) {
+    napi_valuetype type;
+    status = napi_typeof(env, argv[0], &type);
+    CHECK_STATUS;
+    if (type == napi_object) {
+      napi_value property;
+      status = napi_get_named_property(env, argv[0], "showLocalSources", &property);
+      if (status == napi_ok) {
+        status = napi_typeof(env, property, &type);
+        CHECK_STATUS;
+        if (type == napi_boolean) {
+          status = napi_get_value_bool(env, property, &find_create.show_local_sources);
+          CHECK_STATUS;
+          printf("Find create show local sources %i\n", find_create.show_local_sources);
+        }
+      } // status == napi_ok for showLocalSources
+      status = napi_get_named_property(env, argv[0], "groups", &property);
+      if (status == napi_ok) {
+        status = napi_typeof(env, property, &type);
+        CHECK_STATUS;
+        switch (type) {
+          case napi_string:
+            size_t len;
+            status = napi_get_value_string_utf8(env, property, nullptr, 0, &len);
+            CHECK_STATUS;
+            find_create.p_groups = (const char *) malloc(len + 1);
+            status = napi_get_value_string_utf8(env, property,
+              (char *) find_create.p_groups, len + 1, &len);
+            CHECK_STATUS;
+            printf("Find create groups %s\n", find_create.p_groups);
+            break;
+          default:
+            break;
+        }
+      } // status == napi_ok for p_groups
+      status = napi_get_named_property(env, argv[0], "extraIPs", &property);
+      if (status == napi_ok) {
+        status = napi_typeof(env, property, &type);
+        CHECK_STATUS;
+        switch (type) {
+          case napi_string:
+            size_t len;
+            status =  napi_get_value_string_utf8(env, property, nullptr, 0, &len);
+            CHECK_STATUS;
+            find_create.p_extra_ips = (const char *) malloc(len + 1);
+            status = napi_get_value_string_utf8(env, property,
+              (char *) find_create.p_extra_ips, len + 1, &len);
+            CHECK_STATUS;
+            printf("Find create groups %s\n", find_create.p_extra_ips);
+            break;
+          default:
+            break;
+        }
+      }
+
+    } // type == napi_object
+  } // argc >= 1
 
 	// We are going to create an NDI finder that locates sources on the network.
 	NDIlib_find_instance_t pNDI_find = NDIlib_find_create_v2(&find_create);
@@ -50,9 +123,18 @@ napi_value find(napi_env env, napi_callback_info info) {
   CHECK_STATUS;
 
   for ( uint32_t i = 0 ; i < no_sources; i++ ) {
-    napi_value item;
-    status = napi_create_string_utf8(env, p_sources[i].p_url_address, NAPI_AUTO_LENGTH, &item);
+    napi_value name, uri, item;
+    status = napi_create_string_utf8(env, p_sources[i].p_ndi_name, NAPI_AUTO_LENGTH, &name);
     CHECK_STATUS;
+    status = napi_create_string_utf8(env, p_sources[i].p_url_address, NAPI_AUTO_LENGTH, &uri);
+    CHECK_STATUS;
+    status = napi_create_object(env, &item);
+    CHECK_STATUS;
+    status = napi_set_named_property(env, item, "name", name);
+    CHECK_STATUS;
+    status = napi_set_named_property(env, item, "urlAddress", uri);
+    CHECK_STATUS;
+
     status = napi_set_element(env, result, i, item);
     CHECK_STATUS;
   }
@@ -61,7 +143,7 @@ napi_value find(napi_env env, napi_callback_info info) {
 	NDIlib_find_destroy(pNDI_find);
 
 	// Finished
-	// NDIlib_destroy();
+	NDIlib_destroy();
 
   printf("Look who got called!\n");
 
@@ -133,9 +215,10 @@ napi_value Init(napi_env env, napi_value exports) {
   napi_property_descriptor desc[] = {
     DECLARE_NAPI_METHOD("version", version),
     DECLARE_NAPI_METHOD("find", find),
+    DECLARE_NAPI_METHOD("isSupportedCPU", isSupportedCPU),
     DECLARE_NAPI_METHOD("send", send)
    };
-  status = napi_define_properties(env, exports, 3, desc);
+  status = napi_define_properties(env, exports, 4, desc);
   CHECK_STATUS;
 
   return exports;
