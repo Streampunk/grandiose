3b9bd1706f39be441af691fc0764c640e432b65e
Jens Stigaard <jens@stigaard.info>
2020-07-26 15:27:57 +0200

    Cleaned up scratch code

--- scratch/scratchReceiveVideo.js
+++ scratch/scratchReceiveVideo.js
@@ -1,6 +1,6 @@
 /* Copyright 2018 Streampunk Media Ltd.
 
-  Licensed under the Apache License, Version 2.0 (the "License");
+  Licensed under the Apache License, Version 2.0 (the "License")
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
 
@@ -13,27 +13,41 @@
   limitations under the License.
 */
 
-const g = require('../index.js');
+const g = require('../index.js')
 
 async function run() {
-  let l = await g.find();
-  console.log('>>> FOUND >>>', l);
-  let r = await g.receive({ source: l[0] });
-  console.log('>>> RECEIVER >>>', r);
-  for ( let x = 0 ; x < 10 ; x++ ) {
-    let v = await r.video();
-    console.log('>>> VIDEO >>>', v);
-    console.log(process.memoryUsage());
-    v = null;
+  let l = await g.find()
+  console.log('>>> FOUND >>>', l)
+
+  let r = await g.receive({ source: l[0] })
+  console.log('>>> RECEIVER >>>', r)
+
+  for (let x = 0; x < 10; x++) {
+    let v = await r.video()
+    console.log('>>> VIDEO >>>', v)
+    console.log(process.memoryUsage())
+    v = null
   }
-  l = null;
-  r = null;
-  v = null;
-  console.log(process.memoryUsage());
-  setTimeout(() => { global.gc();
-    console.log("that's almost all folks", process.memoryUsage()); }, 1000);
-  setTimeout(() => {   global.gc(); console.log("that's it", process.memoryUsage()); }, 2000);
-  setTimeout(() => {   global.gc(); console.log("that's really it", process.memoryUsage()); }, 3000);
+
+  l = null
+  r = null
+  v = null
+
+  console.log(process.memoryUsage())
+
+  setTimeout(() => {
+    global.gc()
+    console.log("that's almost all folks", process.memoryUsage())
+  }, 1000)
+  setTimeout(() => {
+    global.gc()
+    console.log("that's it", process.memoryUsage())
+  }, 2000)
+
+  setTimeout(() => {
+    global.gc()
+    console.log("that's really it", process.memoryUsage())
+  }, 3000)
 }
 
-run();
+run()
