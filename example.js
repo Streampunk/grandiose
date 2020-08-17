6ac0af0fa19a2bc7f68e78e765a9af20048080e2
Jens Stigaard <jens@stigaard.info>
2020-08-17 13:05:42 +0200

    Simplified example

--- example.js
+++ example.js
@@ -1,34 +1,53 @@
 
 const grandiose = require('./index')
+console.log('NDI version:', grandiose.version())
 
-console.log(grandiose.version())
+// Guard unsupported CPU
+if (!grandiose.isSupportedCPU()) {
+	console.error('Unsupported CPU')
+	return
+}
+
 console.log('Is CPU supported?', grandiose.isSupportedCPU())
 
-const timeout = 5000; // Optional timeout, default is 10000ms
+const timeout = 5000 // Optional timeout, default is 10000ms
+
 
 grandiose.find({
 	// Should sources on the same system be found?
 	showLocalSources: true
 })
 	.then(async sources => {
+		// Guard no sources found
 		if (!sources.length) {
+			console.log('No NDI sources found')
 			return
 		}
-		console.log('Found sources', sources.length)
 
-		let source = sources[0]
+		console.log('Found NDI sources', sources.length)
 
-		console.log('Source', source)
+		const source = sources[0]
 
-		let receiver = await grandiose.receive({ source: source })
+		console.log('NDI Source chosen', source)
 
+		const receiver = await grandiose.receive({ source: source })
+
+		// Read 10 frames
 		for (let x = 0; x < 10; x++) {
 			try {
-				let videoFrame = await receiver.video(timeout)
+				const videoFrame = await receiver.video(timeout)
 				console.log(videoFrame)
 			} catch (e) { console.error(e) }
 		}
 	})
-	.catch(console.error)
+	.catch(e => {
+		if (e.code && e.code.trim() === '4040') {
+			console.error('No NDI sources found...')
+			return
+		}
+
+		console.error('UNKNOWN ERROR...')
+		console.error(e)
+	})
 
 
