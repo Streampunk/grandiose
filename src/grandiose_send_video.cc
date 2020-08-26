80483a6a600f7e575e7533c1cd31234ffb32a448
Jens Stigaard <jens@stigaard.info>
2020-08-26 12:52:20 +0200

    Fixed logic for NDI send example code

--- src/grandiose_send_video.cc
+++ src/grandiose_send_video.cc
@@ -28,7 +28,7 @@
 #include "grandiose_send_video.h"
 #include "grandiose_util.h"
 
-// Send video
+// Send video frame
 napi_value sendVideo(napi_env env, napi_callback_info info)
 {
   napi_status status;
@@ -49,11 +49,18 @@ napi_value sendVideo(napi_env env, napi_callback_info info)
   if (!pNDI_send)
     NAPI_THROW_ERROR("Failed to create send instance.");
 
-  // We are going to create a 1920x1080 interlaced frame at 29.97Hz.
+  // We are going to create a frame with following specs
   NDIlib_video_frame_v2_t NDI_video_frame;
 
+  // - Resolution: 1920x1080
   NDI_video_frame.xres = 1920;
   NDI_video_frame.yres = 1080;
+
+  // - Frame rate: 25Hz (25000/1000)
+  NDI_video_frame.frame_rate_N = 25000;
+  NDI_video_frame.frame_rate_D = 1000;
+
+  // Data delivered in color format: BGRX
   NDI_video_frame.FourCC = NDIlib_FourCC_type_BGRX;
 
   // Calculate number of pixels in video frame
@@ -71,20 +78,28 @@ napi_value sendVideo(napi_env env, napi_callback_info info)
     const auto start_send = high_resolution_clock::now();
 
     // Send 200 frames
-    for (int idx = 200; idx; idx--)
+    for (int frame_index = 200; frame_index; frame_index--)
     {
+      // Guard no connected receivers - timeout at 1000 ms
+      if (!NDIlib_send_get_no_connections(pNDI_send, 1000))
+      {
+        printf("No current connections, so no rendering needed for frame %d.\n", frame_index);
+        continue;
+      }
+
       // Fill in the buffer. It is likely that you would do something much smarter than this.
       // All pixels is painted the same color - the color is switching between black and white based on the frame number
-      memset((void *)NDI_video_frame.p_data, (idx & 1) ? 255 : 0, number_of_pixels * 4);
+      memset((void *)NDI_video_frame.p_data, (frame_index & 1) ? 255 : 0, number_of_pixels * 4);
 
       // We now submit the frame. Note that this call will be clocked so that we end up submitting at exactly 29.97fps.
       NDIlib_send_send_video_v2(pNDI_send, &NDI_video_frame);
     }
+    // Finish looping 200 frames
 
-    // Just display something helpful
+    // Display framerate for generated framerate
     printf("200 frames sent, at %1.2ffps\n", 200.0f / duration_cast<duration<float>>(high_resolution_clock::now() - start_send).count());
   }
-  // Finish looping
+  // Finish looping for desired number of minutes
 
   // Free the video frame
   free(NDI_video_frame.p_data);
